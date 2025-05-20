223e2f3bf7777637e56979bce7418a2653421cae
ftheirs <fntheirs@gmail.com>
2025-05-20 14:22:24 -0300

    fix build errors

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,7 +1,7 @@
-use actix_web::{web, HttpRequest, HttpResponse, Responder};
-use actix_web::http::header::{self, Accept};
+use actix_web::http::header::{self};
+use actix_web::{HttpRequest, HttpResponse, Responder};
+use mime::{Mime, APPLICATION, JSON};
 use serde::Serialize;
-use mime;
 
 // Define the struct for the JSON response
 #[derive(Serialize)]
@@ -12,16 +12,17 @@ struct HelloResponse {
 // Modify the handler signature to accept HttpRequest
 pub async fn hello(req: HttpRequest) -> impl Responder {
     // Check the Accept header
-    let accept_header: Option<Accept> = req.headers().get(header::ACCEPT)
-        .and_then(|hv| hv.to_str().ok())
-        .and_then(|s| s.parse().ok());
+    let accept_header = req
+        .headers()
+        .get(header::ACCEPT)
+        .and_then(|hv| hv.to_str().ok());
 
     // Determine the preferred content type
     let json_preferred = accept_header.map_or(false, |accept| {
-        accept.iter().any(|pref| {
-            let mime = pref.item.as_ref();
-            mime.type_() == mime::APPLICATION && mime.subtype() == mime::JSON
-        })
+        accept
+            .split(',')
+            .filter_map(|s| s.trim().parse::<Mime>().ok())
+            .any(|mime| mime.type_() == APPLICATION && mime.subtype() == JSON)
     });
 
     if json_preferred {
